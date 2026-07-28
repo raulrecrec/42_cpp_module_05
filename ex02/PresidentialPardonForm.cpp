2b2c9d1789197f9f50afcb2cc0016cd698fc65fa
WSL HOME <raulrecrec@gmail.com>
2026-07-28 20:58:28 +0200

    Add PresidentialPardonForm
    - Created PresidentialPardonForm derived from AForm with Orthodox Canonical Form.
    - Implemented the presidential pardon action for the target.
    - Displayed the pardon message issued by Zaphod Beeblebrox.
    - Reused the AForm execution workflow through executeAction().
    - Preserved the canonical behavior by copying the base class state.

---
+++ ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PresidentialPardonForm.cpp                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/28 20:49:08 by rexposit          #+#    #+#             */
+/*   Updated: 2026/07/28 20:58:04 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PresidentialPardonForm.hpp"
+#include <iostream>
+
+PresidentialPardonForm::PresidentialPardonForm()
+	: AForm("PresidentialPardonForm", 25, 5) , _target("Default")
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target)
+	: AForm("PresidentialPardonForm", 25, 5), _target(target)
+{
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other)
+	: _target(other._target)
+{
+}
+
+PresidentialPardonForm	&PresidentialPardonForm::operator=(const PresidentialPardonForm &other)
+{
+	if (this != &other)
+	{
+		AForm::operator=(other);
+		this->_target = other._target;
+	}
+
+	return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+void	PresidentialPardonForm::executeAction() const
+{
+	std::cout << _target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+}
